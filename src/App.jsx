8585470d9a6765b9cae5c101d650a9554d58e90d
FakeGods <w.leszkowicz99@gmail.com>
2024-03-14 14:52:45 +0100

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   );
-}
+};
 
 export default App;
