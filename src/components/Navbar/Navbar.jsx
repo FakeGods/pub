5db7f09ea92aa33af18198e0e73e25fc293aa2ce
FakeGods <w.leszkowicz99@gmail.com>
2024-03-17 20:09:07 +0100

    love

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-import { navItems } from "../constants/index.jsx";
+import { navItems } from "../../constants/index.jsx";
 
 const Navbar = () => {
   return (
