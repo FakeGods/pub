b3b2ed887288c4cd8c4762c8fe63c53403049e0d
FakeGods <w.leszkowicz99@gmail.com>
2024-03-18 14:13:15 +0100

    form contact?

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Contact />
     </>
   );
 };
