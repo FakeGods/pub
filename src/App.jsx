92a3798f1f546e1c0e00864a533a34e4c41ef310
FakeGods <w.leszkowicz99@gmail.com>
2024-03-22 21:09:27 +0100

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar/Navbar";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Services from "./components/services/Services";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
       <Contact />
       <Footer />
       <Services />
