7d015812ed9da3996bc8719a4593cf300046da58
FakeGods <w.leszkowicz99@gmail.com>
2024-03-22 21:12:01 +0100

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Services from "./components/services/Services";
-import Hero from "./components/Hero/Hero";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
   return (
