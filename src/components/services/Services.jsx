3b916924a3271e5c8eda6f93e20ae0bbca581059
FakeGods <w.leszkowicz99@gmail.com>
2024-03-23 22:43:35 +0100

    1

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Services = () => {
-  return <div className="sm:text-center">Services tak</div>;
+  return <div className="sm:text-center">Oferta</div>;
 };
 
 export default Services;
