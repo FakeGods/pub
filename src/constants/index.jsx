252d00f443550184b77392b3a4f209512440fcf0
FakeGods <w.leszkowicz99@gmail.com>
2024-03-26 10:46:03 +0100

    index

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,9 +1,6 @@
 import { BotMessageSquare } from "lucide-react";
-import { BatteryCharging } from "lucide-react";
 import { Fingerprint } from "lucide-react";
 import { ShieldHalf } from "lucide-react";
-import { PlugZap } from "lucide-react";
-import { GlobeLock } from "lucide-react";
 
 export const navItems = [
   { label: "O nas", href: "1" },
